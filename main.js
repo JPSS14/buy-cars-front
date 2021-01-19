01ca791e16c350a30694c0edcbefa53226116085
JPSS14 <jp_scarabelli@hotmail.com>
2021-01-19 02:12:58 -0300

    UPDATE 42: Added show function for each menu cars dropdown

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-function show(){
-    const elemento = document.getElementById("porsche");
+function show(op) {
+    let elementoP;
+    let elementoSecundario1;
+    let elementoSecundario2;
+    if (op == 1) {
+        elementoP = document.getElementById("porsche");
+        elementoSecundario1 = document.getElementById("mclaren");
+        elementoSecundario2 = document.getElementById("lamborghini");
+    }
+    else if (op == 2) {
+        elementoP = document.getElementById("mclaren");
+        elementoSecundario1 = document.getElementById("porsche");
+        elementoSecundario2 = document.getElementById("lamborghini");
+    }
+    else if (op == 3) {
+        elementoP = document.getElementById("lamborghini");
+        elementoSecundario1 = document.getElementById("mclaren");
+        elementoSecundario2 = document.getElementById("porsche");
+    }
+
+    const elemento = elementoP;
     const elementoClasses = elemento.classList;
     const elementoSize = elementoClasses.length;
 
-    console.log(elementoSize);
-    for(let i = 0; i<elementoSize; i++){
-        if(elementoClasses[i] == "show"){
+    for (let i = 0; i < elementoSize; i++) {
+        if (elementoClasses[i] == "show") {
             elemento.classList.remove("show");
-        }else{
+        } else {
             elemento.classList.add("show");
+            elementoSecundario1.classList.remove("show");
+            elementoSecundario2.classList.remove("show");
         }
-        console.log("tes");
     }
-
-    // if(elementoClasses) elemento.classList.add("show");
-    // else elemento.className += " show";
-    // console.log(elemento.classList);
 }
